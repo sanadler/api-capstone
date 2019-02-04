b366473a430ad4a45c881b3829dea3b331608f82
Sarah Nadler <sarah.m.nadler@gmail.com>
2019-02-04 13:52:34 -0700

    comments added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //how to fix submit button on safari ??
 //buttons on mobile
 
+//fetches all pokemon from the api
 function getPokemon() {
   fetch(`https://pokeapi.co/api/v2/pokemon/`)
     .then(handleErrors)
@@ -12,6 +13,7 @@ function getPokemon() {
     .catch(error => alert(error));
 }
 
+//fetches the prevous page of pokemon (limited to 20 per page)
 function getPreviousPage(previous){
   fetch(`${previous}`)
   .then(handleErrors)
@@ -21,6 +23,7 @@ function getPreviousPage(previous){
   .catch(error => alert(error));
 }
 
+//fetches the next page of pokemon (limited to 20 per page)
 function getNextPage(next){
   fetch(`${next}`)
   .then(handleErrors)
@@ -30,6 +33,7 @@ function getNextPage(next){
   .catch(error => alert(error));
 }
 
+//fetches the specific pokemon, based on the pokemon passed to the function
 function getSpecificPokemon(pokemon) {
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
     .then(handleErrors)
@@ -39,6 +43,7 @@ function getSpecificPokemon(pokemon) {
     .catch(error => alert(error));
 }
 
+//fetches all pokemon of the type passed to the function
 function getPokemonByType(type) {
   fetch(`https://pokeapi.co/api/v2/type/${type}/`)
     .then(handleErrors)
@@ -55,26 +60,31 @@ function handleErrors(response) {
     return response;
 }
 
-
 function displayHomepage(responseJson) {
   console.log(responseJson);
   let poke = responseJson.results;
+  //goes through all the pokemon, pulls the id from the URL and uses it to attach the correct image
   for (let i=0; i < poke.length; i++){
     let url = `${poke[i].url}`.split('/');
     let id = url.pop() || url.pop();
+    //adds a grid of pokemon buttons with the name and image
     $('.grid-page').append(`<div class="col-3">
       <div class="box">
       <button type="button" name="poke-card" value="${poke[i].name}"><img class="poke-card" alt="picture of ${poke[i].name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" onerror="this.src='poke-ball.png'"><br>${poke[i].name}</button></div>
     </div>`);
   }
+  //if you are on the first page, the previous button won't pop up .. otherwise adds a button to get to the previous page of pokemon
   if(responseJson.previous != null){
     $('.prev-next').append(`<div class="col-6"><button type="button" name="previous">Previous</button></div>`);
   }
+
+   //if you are on the last page, the next button won't pop up .. otherwise adds a button to get to the next page of pokemon
   if(responseJson.next != null){
     $('.prev-next').append(`<div class="col-6"><button type="button" name="next">Next</button></div>`);
   }
-
+ //displays results
   $('.results').removeClass('hidden');
+  //watches all the buttons, performs correct ask once button is clicked
   $('button').click(function(){
     if(this.name === 'poke-card'){
       let pokemon = $(this).attr('value');
@@ -91,6 +101,7 @@ function displayHomepage(responseJson) {
   });
 }
 
+//displays pokemon the same way as the homepage but adds a header saying which type is being displayed
 function displayPokemonByType(responseJson) {
   console.log(responseJson.name);
   let poke = responseJson.pokemon;
@@ -113,6 +124,7 @@ function displayPokemonByType(responseJson) {
   });
 }
 
+//displays the stats of the specific pokemon selected
 function displayPokemon(responseJson){
   console.log(responseJson);
   clearPage();
@@ -143,6 +155,7 @@ function displayPokemon(responseJson){
     watchPokePageButtons();
 }
 
+//watches the form, if a pokemon is searched for the app displays the stats of the specific pokemon
 function watchForm() {
   $('form').submit(event => {
     let pokemon = $('#pokemon-search').val();
@@ -153,6 +166,8 @@ function watchForm() {
   });
 }
 
+//watches the buttons on the pokemon page of stats. if homepage button is clicked, you are sent to homepage
+//if a type button is clicked you are sent to page of pokemon of that type
 function watchPokePageButtons(){
   $('button').click(function(){
     if(this.name === 'back'){
@@ -167,6 +182,7 @@ function watchPokePageButtons(){
   });
 }
 
+//clears the page so that the next page can be diplayed alone
 function clearPage(){
   $('.row').empty();
 }
